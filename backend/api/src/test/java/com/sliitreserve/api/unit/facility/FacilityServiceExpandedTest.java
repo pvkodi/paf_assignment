d3c2f7eee3e6efa084b30933db8414dcf9e35414
Pamali Kodi <161586406+pvkodi@users.noreply.github.com>
2026-04-14 12:50:59 +0530

    Fix facility test verification and add EndpointAuthorizationFilter

--- backend/api/src/test/java/com/sliitreserve/api/unit/facility/FacilityServiceExpandedTest.java
+++ backend/api/src/test/java/com/sliitreserve/api/unit/facility/FacilityServiceExpandedTest.java
@@ -1,8 +1,9 @@
 package com.sliitreserve.api.unit.facility;
 
 import com.sliitreserve.api.entities.facility.*;
+import com.sliitreserve.api.dto.facility.FacilityRequestDTO;
 import com.sliitreserve.api.factories.FacilityFactory;
-import com.sliitreserve.api.repositories.FacilityRepository;
+import com.sliitreserve.api.repositories.facility.FacilityRepository;
 import com.sliitreserve.api.services.facility.FacilityService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,22 +31,40 @@ public class FacilityServiceExpandedTest {
 
     @Test
     public void factory_creates_correct_subclasses() {
-        Facility f1 = FacilityFactory.create(Facility.FacilityType.LECTURE_HALL, "L1", "Hall", 100, "B1");
+        FacilityRequestDTO r1 = new FacilityRequestDTO();
+        r1.setType(Facility.FacilityType.LECTURE_HALL);
+        r1.setFacilityCode("L1");
+        r1.setName("Hall");
+        r1.setCapacity(100);
+        r1.setBuilding("B1");
+        Facility f1 = new FacilityFactory().createFacility(r1);
         assertTrue(f1 instanceof LectureHall);
 
-        Facility f2 = FacilityFactory.create(Facility.FacilityType.LAB, "LB1", "Chem Lab", 30, "B2");
+        FacilityRequestDTO r2 = new FacilityRequestDTO();
+        r2.setType(Facility.FacilityType.LAB);
+        r2.setFacilityCode("LB1");
+        r2.setName("Chem Lab");
+        r2.setCapacity(30);
+        r2.setBuilding("B2");
+        Facility f2 = new FacilityFactory().createFacility(r2);
         assertTrue(f2 instanceof Lab);
 
-        Facility f3 = FacilityFactory.create(Facility.FacilityType.EQUIPMENT, "E1", "Projector", 1, "Store");
+        FacilityRequestDTO r3 = new FacilityRequestDTO();
+        r3.setType(Facility.FacilityType.EQUIPMENT);
+        r3.setFacilityCode("E1");
+        r3.setName("Projector");
+        r3.setCapacity(1);
+        r3.setBuilding("Store");
+        Facility f3 = new FacilityFactory().createFacility(r3);
         assertTrue(f3 instanceof Equipment);
     }
 
     @Test
     public void searchFacilities_callsRepository_with_null_spec_when_no_filters() {
-        when(facilityRepository.findAll((Specification<Facility>) null)).thenReturn(List.of());
+        when(facilityRepository.findAll()).thenReturn(List.of());
         List<Facility> res = facilityService.searchFacilities(null, null, null, null, null);
         assertNotNull(res);
-        verify(facilityRepository).findAll((Specification<Facility>) null);
+        verify(facilityRepository).findAll();
     }
 
     @Test
